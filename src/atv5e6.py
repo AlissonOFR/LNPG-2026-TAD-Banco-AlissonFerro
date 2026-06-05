ee76554f3d6f32390f9b35b78a53866023b134d0
AlissonOFR <152882782+AlissonOFR@users.noreply.github.com>
2026-06-05 18:46:07 -0300

    Add banking system implementation in atv5e6.py

---
+++ src/atv5e6.py
@@ -0,0 +1,50 @@
+from banco import Banco
+from cliente import Cliente
+from contaBancaria import contaBancaria
+
+banco = Banco("Banco IFAL", 1001)
+
+
+cliente1 = Cliente("João Lucas","01234566789","82987641360","joao@gmail.com")
+cliente2 = Cliente("Vitor","98765432101","85999557720","vitor@gmailcom")
+cliente3 = Cliente("Ana Gabriela","24681357901","21988562301","ana@gmail.com")
+
+conta1 = contaBancaria(1, cliente1, banco, 1000.00)
+conta2 = contaBancaria(2, cliente2, banco, 5000.00)
+conta3 = contaBancaria(3, cliente3, banco, 900.00)
+conta4 = contaBancaria(4, cliente3, banco, 8000.00)
+
+banco.criar_contas(conta1)
+banco.criar_contas(conta2)
+banco.criar_contas(conta3)
+banco.criar_contas(conta4)
+
+conta1.depositar(300)
+conta2.depositar(400)
+conta3.depositar(1000)
+conta4.depositar(100)
+
+conta1.saque(100)
+conta2.saque(200)
+conta3.saque(150)
+conta4.saque(600)
+
+conta1.consultarSaldo
+conta2.consultarSaldo
+conta3.consultarSaldo
+conta4.consultarSaldo
+
+print(f"Banco: {banco.nomeBanco}")
+print(f"Código do Banco: {banco.codigo}")
+print(f"Quantidade de Contas: {banco.quantidade_contas()}")
+saldoTotal = 0
+for conta in banco.listar_contas():
+    print(f"Cliente: {conta.getTitular().getNome()}")
+    print(f"CPF: {conta.getTitular().getCpf()}")
+    print(f"Telefone: {conta.getTitular().getTelefone()}")
+    print(f"Email: {conta.getTitular().getEmail()}")
+    print(f"Conta: {conta.getNumero()}")
+    print(f"Saldo: R$ {conta.consultarSaldo():.2f}")
+
+    saldoTotal += conta.consultarSaldo()
+print(f"Saldo total do banco: R$ {saldoTotal:.2f}")
